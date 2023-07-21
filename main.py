7531699ed59f0135af41abc11fa98cf4beb01ec1
Sanskriti Gupta <112421586+SanskritiGupta-03@users.noreply.github.com>
2023-07-22 01:33:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+from rembg import remove
+import time
+input_path = "oldpic.JPG"
+output_path = "new.jpg"
+
+start_time = time.time()
+with open(input_path, "rb") as i:
+    with open(output_path, "wb") as o:
+        input = i.read()
+        output = remove(input)
+        o.write(output)
+        end_time = time.time()
+print(f"Image background is removed in {str(end_time - start_time)[:3]}")
+
